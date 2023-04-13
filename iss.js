8a496096db00c8d65a7e699d31c67be49204f563
mfried95 <m.friedman1995@gmail.com>
2023-04-13 16:33:16 -0400

    Added 4rd fucntion

--- iss.js
+++ iss.js
@@ -56,9 +56,8 @@ const fetchCoordsByIP = function(ip, callback) {
 
 
 const fetchISSFlyOverTimes = function(coords, callback) {
-  
-  let url = `https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
-  
+  const url = `https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
+
   request(url, (error, response, body) => {
     if (error) {
       callback(error, null);
@@ -75,7 +74,28 @@ const fetchISSFlyOverTimes = function(coords, callback) {
   });
 };
 
+const nextISSTimesForMyLocation = function(callback) {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    fetchCoordsByIP(ip, (error, loc) => {
+      if (error) {
+        return callback(error, null);
+      }
+
+      fetchISSFlyOverTimes(loc, (error, nextPasses) => {
+        if (error) {
+          return callback(error, null);
+        }
+
+        callback(null, nextPasses);
+      });
+    });
+  });
+};
 
 
 
-module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes  };
+module.exports = { fetchISSFlyOverTimes  };
